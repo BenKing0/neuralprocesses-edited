f328bbf4f3d2ed79c4ec3b672cd5776caf731fac
BenKing0 <73542437+BenKing0@users.noreply.github.com>
2022-06-21 17:28:09 +0100

    Add files via upload

--- correlation_significance.py
+++ correlation_significance.py
@@ -131,10 +131,13 @@ def run(config):
     model = build_corrconvnp(config)
     model = model.to(device)
     opt = torch.optim.Adam(model.parameters(), config.rate)
+    vals_corr, vals_corr_cv = [], []
     for i in range(0, config.epochs):
         with out.Section(f"Epoch {i + 1}"):
-            _, vals_corr = train(state, model, opt, objective, data_generator)
-            _, vals_corr_cv = eval(state, model, objective, data_generator)
+            _, _val_corr = train(state, model, opt, objective, data_generator)
+            _, _val_corr_cv = eval(state, model, objective, data_generator)
+            vals_corr.append(_val_corr)
+            vals_corr_cv.append(_val_corr_cv)
 
     torch.save(
         {
@@ -149,17 +152,6 @@ def run(config):
         wd.file(f"model-last-corrconvnp-decoder.torch"),
     )
 
-    # _keep_keys = dict()
-    # for key, value in model.state_dict().items():
-    #     _keep_keys[key] = value
-
-    # torch.save(
-    #     {
-    #         "weights": model.state_dict(),
-    #     },
-    #     wd.file(f"model-last-corrconvnp.torch"),
-    # )
-
     model = build_convnp(config)
     model = model.to(device)
 
@@ -170,21 +162,15 @@ def run(config):
         torch.load(wd.file("model-last-corrconvnp-decoder.torch"), map_location=device)["weights"]
     )
 
-    # i = 0
-    # for og_key, _ in _keep_keys.items():
-    #     _unused_key = list(model.state_dict().keys())[i]
-    #     model.state_dict()[og_key] = model.state_dict().pop(_unused_key)
-    #     i += 1
-
-    # model.load_state_dict(
-    #     torch.load(wd.file("model-last-corrconvnp.torch"), map_location=device)["weights"]
-    # )
-
     opt = torch.optim.Adam(model.parameters(), config.rate)
+    vals, vals_cv = [], []
     for i in range(config.epochs, 2 * config.epochs):
         with out.Section(f"Epoch {i + 1}"):
-            _, vals = train(state, model, opt, objective, data_generator)
-            _, vals_cv = eval(state, model, objective, data_generator)
+            _, _val = train(state, model, opt, objective, data_generator)
+            _, _val_cv = eval(state, model, objective, data_generator)
+            vals.append(_val)
+            vals_cv.append(_val_cv)
+
     torch.save(
         {
             "weights": model.state_dict(),
@@ -202,12 +188,12 @@ def plot(vals_corr, vals_corr_cv, vals, vals_cv, wd):
     linear_means = -np.array([vals_corr[0], vals[0]]).flatten()
     linear_vars = np.array([vals_corr[1], vals[1]]).flatten()
     plt.plot(xs, linear_means, 'xkcd:royal blue')
-    plt.fill_between(xs, linear_means - linear_vars, linear_means + linear_vars, 'xkcd:royal blue', alpha=0.4)
+    plt.fill_between(xs, linear_means - linear_vars, linear_means + linear_vars, color='xkcd:royal blue', alpha=0.4)
 
     cv_linear_means = -np.array([vals_corr_cv[0], vals_cv[0]]).flatten()
     cv_linear_vars = np.array([vals_corr_cv[1], vals_cv[1]]).flatten()
     plt.plot(xs, cv_linear_means, 'xkcd:red')
-    plt.fill_between(xs, cv_linear_means - cv_linear_vars, cv_linear_means + cv_linear_vars, 'xkcd:red', alpha=0.4)
+    plt.fill_between(xs, cv_linear_means - cv_linear_vars, cv_linear_means + cv_linear_vars, color='xkcd:red', alpha=0.4)
 
     plt.title('Training loss with epochs')
     plt.plot(np.ones(100)*len(linear_means)//2, np.linspace(min(linear_means), max(linear_means), 100), 'k--', alpha=0.4)
@@ -226,13 +212,13 @@ if __name__ == '__main__':
         'dim_yc': 1,
         'dim_lv': 1, 
         'dim_yt': 1,
-        'epochs': 1,
+        'epochs': 10,
         'arch': 'unet',
         'objective': 'elbo', ## HC
         'mode': '_train', ## HC 
         'data': 'eq',    
         'batch_size': 16,
-        'train_test': True,    
+        'train_test': False,    
         'rate': 3e-4,
         'num_samples': 20,
         "width": 256,
@@ -250,120 +236,3 @@ if __name__ == '__main__':
     vals_corr, vals_corr_cv, vals, vals_cv, wd = run(config)
 
     plot(vals_corr, vals_corr_cv, vals, vals_cv, wd)
-
-
-
-# def _build_adapted_convnp(dim_lv, dim_x, dim_y, dim_yc, dim_yt, unet_channels, unet_kernels, points_per_unit):
-
-#     dim_yc, dim_yt, conv_in_channels = nps.architectures.convgnp._convgnp_init_dims(dim_yc, dim_yt, dim_y)
-#     num_basis_functions = 64
-
-#     lv_likelihood_in_channels, _, lv_likelihood = nps.architectures.util.construct_likelihood(
-#         nps,
-#         spec='lowrank',
-#         dim_y=dim_lv,
-#         num_basis_functions=num_basis_functions,
-#         dtype=None,
-#     )
-#     encoder_likelihood = lv_likelihood
-
-#     likelihood_in_channels, selector, likelihood = nps.architectures.util.construct_likelihood(
-#         nps,
-#         spec='het',
-#         dim_y=dim_yt,
-#         num_basis_functions=num_basis_functions,
-#         dtype=None,
-#     )
-
-#     conv_out_channels = nps.architectures.convgnp._convgnp_resolve_architecture(
-#         'unet',
-#         unet_channels,
-#         conv_channels=64,
-#         conv_receptive_field=None,
-#     )
-
-#     if conv_out_channels < likelihood_in_channels:
-#         linear_after_set_conv = nps.Linear(
-#             in_channels=conv_out_channels,
-#             out_channels=likelihood_in_channels,
-#             dtype=None,
-#         )
-#     else:
-#         conv_out_channels = likelihood_in_channels
-#         linear_after_set_conv = lambda x: x
-
-#     lv_in_channels = conv_in_channels
-#     lv_out_channels = lv_likelihood_in_channels
-#     in_channels = dim_lv
-#     out_channels = conv_out_channels  
-#     lv_conv = nps.UNet(
-#         dim=dim_x,
-#         in_channels=lv_in_channels,
-#         out_channels=lv_out_channels,
-#         channels=unet_channels,
-#         kernels=unet_kernels,
-#         strides=2,
-#         activations=None,
-#         resize_convs=False,
-#         resize_conv_interp_method='nearest',
-#         dtype=None,
-#     )
-
-#     conv = nps.UNet(
-#         dim=dim_x,
-#         in_channels=in_channels,
-#         out_channels=out_channels,
-#         channels=unet_channels,
-#         kernels=unet_kernels,
-#         strides=2,
-#         activations=None,
-#         resize_convs=False,
-#         resize_conv_interp_method='nearest',
-#         dtype=None,
-#     )
-#     receptive_field = conv.receptive_field / 64
-
-#     disc = nps.Discretisation(
-#         points_per_unit=points_per_unit,
-#         multiple=2**conv.num_halving_layers,
-#         margin=0.1,
-#         dim=dim_x,
-#     )
-
-#     model = nps.Model(
-#         nps.FunctionalCoder(
-#             disc,
-#             nps.Chain(
-#                 nps.PrependDensityChannel(),
-#                 nps.architectures.convgnp._convgnp_construct_encoder_setconvs(
-#                     nps,
-#                     None,
-#                     dim_yc,
-#                     disc,
-#                     None,
-#                 ),
-#                 nps.architectures.convgnp._convgnp_optional_division_by_density(nps, True, 1e-4),
-#                 nps.Materialise(),
-#                 lv_conv,
-#                 encoder_likelihood, 
-#                 _encoder_diag_transform, ##TODO: force this to be diagonal - to be done in coding stage when model called? Or with new part to chain? Or in model.__call__?
-#             ),
-#         ),
-#         nps.Chain(
-#             conv,
-#             nps.RepeatForAggregateInputs(
-#                 nps.Chain(
-#                     nps.architectures.convgnp._convgnp_construct_decoder_setconv(nps, None, disc, None),
-#                     linear_after_set_conv,
-#                     selector,  # Select the right target output.
-#                 )
-#             ),
-#             likelihood,
-#             nps.architectures.util.parse_transform(nps, transform=None),
-#         ),
-#     )
-
-#     out.kv("Receptive field", receptive_field)
-#     model.receptive_field = receptive_field
-
-#     return model
