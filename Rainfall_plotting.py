535a8a259f45ecdec95cdc7df80f3d00f6efcce0
BenKing0 <73542437+BenKing0@users.noreply.github.com>
2022-07-22 15:10:09 +0100

    Add files via upload

--- Rainfall_plotting.py
+++ Rainfall_plotting.py
@@ -21,6 +21,7 @@ def rainfall_plotter(
     ):
     sns.set_theme()
 
+    # NOTE: batch has shape (b, c, n) for each dict value
     with B.on_device(device):
         epoch = generator.epoch()
         batch = epoch[0]    
@@ -30,10 +31,10 @@ def rainfall_plotter(
     hmap_x1 = np.linspace(xbounds[0], xbounds[1], num)
     hmap_x2 = np.linspace(ybounds[0], ybounds[1], num)
     hmap_X = np.meshgrid(hmap_x1, hmap_x2)
-    hmap_X = np.array(hmap_X).reshape((2, num**2)) # of shape (2, num, num) = (coords, xs, ys) to (2, num^2)
-    yc_bernoulli, yc_precip = batch['yc']
-    yc_bernoulli, yc_precip = yc_bernoulli.reshape(1, -1), yc_precip.reshape(1, -1)
-    xc = batch['xc']
+    hmap_X = np.array(hmap_X).reshape((1, 2, num**2)) # (2, num, num) = (coords, xs, ys) to (1, 2, num^2)
+    yc_bernoulli, yc_precip = batch['yc'][0:1, 0:1, :], batch['yc'][0:1, 1:2, :] # (b, 2, n) to (1, 1, n) each
+    yc_bernoulli, yc_precip = yc_bernoulli.reshape(1, 1, -1), yc_precip.reshape(1, 1, -1)
+    xc = batch['xc'][0:1] # (b, 2, n) to (1, 2, n)
 
     with torch.no_grad():
         _, hmap_dist = model(
@@ -41,29 +42,27 @@ def rainfall_plotter(
             [(xc, yc_bernoulli), (xc, yc_precip)],
             B.cast(torch.float32, hmap_X),
             )
-    hmap_bernoulli = B.to_numpy(hmap_dist.bernoulli_prob[0, 0])
-    hmap_kappa, hmap_chi = hmap_dist.z_gamma[0] # (num, ), (num, ) 
+    hmap_bernoulli = B.to_numpy(hmap_dist.bernoulli_prob) # (1, 1, num^2)
+    hmap_kappa, hmap_chi = hmap_dist.z_gamma[..., 0:1, :], hmap_dist.z_gamma[..., 1:2, :] # both (1, 1, num^2)
 
     # TODO: must be faster way to do this
-    hmap_rain = np.zeros(len(hmap_bernoulli))
-    for i, (kappa_i, chi_i, bern_i) in enumerate(zip(hmap_kappa, hmap_chi, hmap_bernoulli)):
+    hmap_rain = np.zeros(hmap_bernoulli.shape[-1])
+    for i, (kappa_i, chi_i, bern_i) in enumerate(zip(hmap_kappa.reshape(-1), hmap_chi.reshape(-1), hmap_bernoulli.reshape(-1))):
         if bern_i > 0.5:
             # TODO: should this be a mean over samples?
             sample_i = np.mean(np.random.gamma(shape=kappa_i.detach().cpu().numpy(), scale=chi_i.detach().cpu().numpy(), size=num_samples))
             hmap_rain[i] = sample_i
         else:
             hmap_rain[i] = 0
-    hmap_rain = hmap_rain.reshape((num, num)) # (1, num ^ 2) for 1 output dimension and _num points: (1, num ^ 2) => (num ^ 2, ) when selecting hmap_class
+    hmap_rain = hmap_rain.reshape((num, num)) # (num^2,) => (num, num)
 
     # Plot the Heatmap with generated precipitation predictions over gridded inputs, with or without the true precipitation next to it for comparison
     if reference:
         _, (ax1, ax2) = plt.subplots(1, 2, sharex = True, sharey = True, figsize = (10,20))
-        plot1 = ax1.imshow(hmap_rain, cmap='Pastel2', alpha=0.5, vmin=0, vmax=np.max(B.to_numpy(batch['reference'])), extent=[xbounds[0], xbounds[1], ybounds[0], ybounds[1]])
-        ax1.scatter(B.to_numpy(batch['xc'])[0], B.to_numpy(batch['xc'])[1], marker = 'o', color='k', label='Context Points', s=0.1)
-        # ax1.scatter(B.to_numpy(batch['xt'])[0], B.to_numpy(batch['xt'])[1], marker = '+', color='k', label='Target Points', s=0.1)
+        plot1 = ax1.imshow(hmap_rain, cmap='Pastel2', alpha=0.5, vmin=0, vmax=np.max(B.to_numpy(batch['reference'][0])), extent=[xbounds[0], xbounds[1], ybounds[0], ybounds[1]])
+        ax1.scatter(B.to_numpy(batch['xc'])[0, 0], B.to_numpy(batch['xc'])[0, 1], marker = 'o', color='k', label='Context Points', s=0.1)
         ax1.set_title(f'Model Predicted Rainfall')
-        # ax1.legend()
-        plot2 = ax2.imshow(batch['reference'], alpha = 0.5, cmap='Pastel2', vmin=0, vmax=np.max(B.to_numpy(batch['reference'])), extent=[xbounds[0], xbounds[1], ybounds[0], ybounds[1]])
+        plot2 = ax2.imshow(batch['reference'][0], alpha = 0.5, cmap='Pastel2', vmin=0, vmax=np.max(B.to_numpy(batch['reference'][0])), extent=[xbounds[0], xbounds[1], ybounds[0], ybounds[1]])
         ax2.set_title(f'True Rainfall')
         plt.colorbar(plot1, ax=ax1, shrink=0.15)
         plt.colorbar(plot2, ax=ax2, shrink=0.15)
@@ -71,9 +70,7 @@ def rainfall_plotter(
         plt.close()
     else:
         plt.imshow(hmap_rain, cmap='Pastel2', alpha=0.5, vmin=0)
-        plt.scatter(B.to_numpy(batch['xc'])[0], B.to_numpy(batch['xc'])[1], marker = 'o', color='k', label='Context Points', s=0.1)
-        # plt.scatter(B.to_numpy(batch['xt'])[0], B.to_numpy(batch['xt'])[1], marker = '+', color='k', label='Target Points', s=0.1)
+        plt.scatter(B.to_numpy(batch['xc'])[0, 0], B.to_numpy(batch['xc'])[0, 1], marker = 'o', color='k', label='Context Points', s=0.1)
         plt.colorbar()
-        # plt.legend()
         plt.savefig(save_path, bbox_inches = 'tight', dpi = 300)
         plt.close()
